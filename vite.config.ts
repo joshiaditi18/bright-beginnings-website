5be7ffcdcf3d586967d50a67f17ba4bc35ae1c54
joshiaditi18 <aditi.joshi24@pccoepune.org>
2026-03-07 00:01:24 +0530

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,10 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/bright-beginnings-website/",
+  base: "./",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
 });
-
