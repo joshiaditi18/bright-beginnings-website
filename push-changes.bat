76bb8f0d8032f6e6ac4982f4602701f6503f3fd9
joshiaditi18 <aditi.joshi24@pccoepune.org>
2026-03-06 23:26:16 +0530

    Configure for GitHub Pages deployment

---
+++ push-changes.bat
@@ -0,0 +1,6 @@
+@echo off
+cd /d "c:\Users\aditi\OneDrive\Desktop\preschool\bright-beginnings-website"
+git add .
+git commit -m "Configure for GitHub Pages deployment"
+git push
+pause
---
+++ run-git.ps1
@@ -0,0 +1,2 @@
+cd 'c:\Users\aditi\OneDrive\Desktop\preschool\bright-beginnings-website'
+git commit -F commit-msg.txt
